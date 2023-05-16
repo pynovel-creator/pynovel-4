668c6383c5e5ff8264d84b2943c70242d2a7424b
pynovel-creator <130883394+pynovel-creator@users.noreply.github.com>
2023-05-16 16:58:34 +0700

    Delete 4.2 directory

--- 4.2/script/audio/__init__.py
+++
@@ -1,4 +0,0 @@
-import script.audio
-import script.audio.audio
-import script.audio.music
-import script.audio.sound
--- 4.2/script/audio/music.py
+++
@@ -1,9 +0,0 @@
-import pygame.mixer
-pygame.mixer.init()
-
-def play(filename, loops=0):
-    pygame.mixer.music.load('game/' + filename)
-    pygame.mixer.music.play(loops)
-
-def stop():
-    pygame.mixer.music.stop()
